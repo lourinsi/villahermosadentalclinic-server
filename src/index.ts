3747ec80d7cf65ad555bd4c66cee31ce391bde00
Lorenz <19100121@usc.edu.ph>
2026-05-26 17:34:11 +0800

    fixes on errors

--- src/index.ts
+++ src/index.ts
@@ -44,7 +44,7 @@ const corsOptions: cors.CorsOptions = {
 app.use(cors(corsOptions));
 
 // Explicitly handle preflight requests
-app.options('*', cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
